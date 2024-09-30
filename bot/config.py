798c004e9fd862e0087ccf91c6b70f380358d7cb
animepahe69 <tanjirokamado66999@gmail.com>
2024-09-30 13:51:02 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,13 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("20860620", cast=int)
+    API_HASH = config("25d2343b36fc5aea3604c6c50a8e2b59")
+    BOT_TOKEN = config("7239354704:AAFFLkvlQXVKJM76cCJMsSTXnTmSWo6JqpQ")
     DEV = 1287276743
-    OWNER = config("OWNER")
+    OWNER = config("6199677027")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+    THUMB = config("https://envs.sh/wyy.jpg")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
